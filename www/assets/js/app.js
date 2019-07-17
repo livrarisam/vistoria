71dcdf80d4793c7c85704eeb4548a2ebb67ad179
Samuel Xavier <samuel@elav.net.br>
2019-07-17 15:38:06 -0300

    camera

--- www/assets/js/app.js
+++ www/assets/js/app.js
@@ -2645,3 +2645,44 @@ function resizePhotoAnalise(file, file_name, input){
 	reader.readAsDataURL(file);
 
 }
+
+function setOptions(srcType) {
+    var options = {
+        // Some common settings are 20, 50, and 100
+        quality: 50,
+        destinationType: Camera.DestinationType.FILE_URI,
+        // In this app, dynamically set the picture source, Camera or photo gallery
+        sourceType: srcType,
+        encodingType: Camera.EncodingType.JPEG,
+        mediaType: Camera.MediaType.PICTURE,
+        allowEdit: true,
+        correctOrientation: true  //Corrects Android orientation quirks
+    }
+    return options;
+}
+
+function openCamera() {
+
+    var srcType = Camera.PictureSourceType.CAMERA;
+    var options = setOptions(srcType);
+    var func = createNewFileEntry;
+
+    navigator.camera.getPicture(function cameraSuccess(imageUri) {
+
+        displayImage(imageUri);
+        // You may choose to copy the picture, save it somewhere, or upload.
+        func(imageUri);
+
+    }, function cameraError(error) {
+        alert("Unable to obtain picture: " + error);
+
+    }, options);
+}
+
+function displayImage(imgUri) {
+
+    var elem = document.getElementById('imageFile');
+    elem.src = imgUri;
+    $("#imageFile").show();
+
+}
